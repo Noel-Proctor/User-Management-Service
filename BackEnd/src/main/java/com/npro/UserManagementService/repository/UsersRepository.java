b7bbf678fe2be14729e2d4316d5dd36e1eabeeb4
Noel Proctor <noelproctor@hotmail.co.uk>
2025-04-24 21:29:23 +0100

    Project Structure

---
+++ BackEnd/src/main/java/com/npro/UserManagementService/repository/UsersRepository.java
@@ -0,0 +1,10 @@
+package com.npro.UserManagementService.repository;
+
+import com.npro.UserManagementService.model.User;
+import com.npro.UserManagementService.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Integer> {
+}
